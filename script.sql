ef15cddec9f25a03eb877e41bf2e671b4d440492
smartcoding <47672245+itsmydevops@users.noreply.github.com>
2019-08-25 11:07:08 +0530

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,4 @@
+create table portal.employee (employee_id int, employee_name varchar(50),employee_city varchar(50));
+insert into portal.employee (employee_id,employee_name,employee_city) values (111,'Srini','India');
+insert into portal.employee (employee_id,employee_name,employee_city) values (222,'Kushi','America');
+insert into portal.employee (employee_id,employee_name,employee_city) values (333,'Siri','Australia');
